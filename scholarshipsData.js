b4502b455ba5f1a31638a932522bd7a549e262b7
sanjeetkumar803108-del <sanjeetkumar803108@gmail.com>
2026-07-06 11:52:08 +0530

    Create scholarshipsData.js

---
+++ scholarshipsData.js
@@ -0,0 +1,27 @@
+// scholarshipsData.js
+
+export const masterScholarshipData = `
+
+[SCHOLARSHIP 1: AICTE SWANATH SCHOLARSHIP (NSP)]
+
+Eligibility:
+To be eligible, an applicant must:-
+be an Indian national
+be a ward impacted by COVID-19 (i.e., lost one or both parents...) or martyred...
+be enrolled full-time in the 1st, 2nd, 3rd, or 4th year of a regular degree or diploma...
+have a family income of less than ₹8,00,000 per annum
+Note:- Applicants must not be receiving any other scholarship.
+
+Benefits:
+Selected candidates will receive a lump sum of ₹50,000 per year...
+
+Documents:
+For Orphan Candidates: Death certificates, Bonafide, Class 10/12 marksheet...
+(aur baaki documents ka text)
+
+---------------------------------------------------
+
+[SCHOLARSHIP 2: HDFC BANK PARIVARTAN]
+(Yahan dusri scholarship ka text aise hi paste kar do)
+
+`;
